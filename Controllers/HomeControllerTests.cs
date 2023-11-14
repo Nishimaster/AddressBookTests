862caababf4b973f885f0b45832666bde2c8d0ee
Nishimaster <droodzisapro@gmail.com>
2023-11-14 15:14:36 +0000

    initial commit
    test (singular) for address book project - to do more

---
+++ Controllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using AddressBook.Controllers;
+using AddressBook.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using AddressBookTests.Mocks;
+
+namespace AddressBookTests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void List_ReturnsAllAddresses()
+        {
+            //arrange
+
+            var mockAddressRepository = RepositoryMocks.GetAddressRepository();
+            var homeController = new HomeController(mockAddressRepository.Object);
+
+            //act
+
+            var result = homeController.Index();
+
+            //assert
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var addressListViewModel = Assert.IsAssignableFrom<AddressBookEntryListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(2, addressListViewModel.AddressBookEntries.Count());
+        }
+    }
+}
---
+++ Mocks/RepositoryMocks.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using AddressBook.Models.AddressBook;
+
+namespace AddressBookTests.Mocks
+{
+    public class RepositoryMocks
+    {
+        public static Mock<IAddressBookRepository> GetAddressRepository()
+        {
+            var addresses = new List<AddressBookEntry>
+            {
+                new AddressBookEntry
+                {
+                    first_name = "David",
+                    last_name = "Skowron",
+                    phone_number = "07712543092",
+                    email = "test@test.com"
+                },
+                new AddressBookEntry
+                {
+                    first_name = "David2",
+                    last_name = "Skowron2",
+                    phone_number = "077125430922",
+                    email = "test@potato.com"
+                }
+            };
+
+            var mockAddressRepository = new Mock<IAddressBookRepository>();
+            mockAddressRepository.Setup(repo => repo.AllAddresses()).Returns(addresses);
+
+            return mockAddressRepository;
+        }
+    }
+}
